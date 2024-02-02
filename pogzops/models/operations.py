9aad89b3e4cf11b5dab08dd4de1cc54eed664e25
romaintailhurat <romain.tailhurat@gmail.com>
2024-02-02 09:16:50 +0100

    Cleaning operations.py

--- pogzops/models/operations.py
+++ pogzops/models/operations.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 from pogzops.models.envs import PoguesEnv
 from pogzops.models.types import Stamp
-from pogzops.models.status import Status, Failure
+from pogzops.models.status import Status
 from pogzops.remote.opz import change_stamp
 from pogzops.remote.get import get_questionnaire
 import abc
@@ -21,28 +21,6 @@ class SingleQuestionnaireParams(OperationParams):
 @dataclass
 class MultiQuestionnairesParams(OperationParams):
     ids: list[str]
-
-
-"""
-@dataclass
-class Operation:
-    name: str
-    env: PoguesEnv
-    params: SingleQuestionnaireParams
-
-    def execute(self) -> Status:
-        # choose operations from from pogzops.remote.opz for example
-        # then execute
-        # return Status ?
-        match self.name:
-            case "change_stamp":
-                status = change_stamp(self.params.id, self.params.stamp, self.env)
-                print(status)
-                return status
-            case _:
-                print(f"Operation {self.name} execution - to be implemented")
-                return Failure(999)
-"""
 
 
 @dataclass
